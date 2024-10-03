9c3a41212d64573b09d828b66fc6444cb921182a
AlexMili <AlexMili@users.noreply.github.com>
2024-10-03 17:04:40 +0200

    Add package version

--- src/reachable/__init__.py
+++ src/reachable/__init__.py
@@ -1,3 +1,11 @@
+import os.path as osp
+
 from reachable.main import is_reachable, is_reachable_async
 
+
 __all__ = ["is_reachable", "is_reachable_async"]
+
+version_path = osp.join(osp.dirname(__file__), "VERSION.md")
+if osp.exists(version_path):
+    with open(version_path, "r") as f:
+        __version__ = f.readline()
